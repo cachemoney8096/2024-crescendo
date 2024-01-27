a9df3d600c43f50cac4f8d5923ba03ddfbb29769
Asher <asher.grossman@gmail.com>
2024-01-27 15:02:10 -0600

    feat: Add relative encoder and limit switch functionality to Conveyer subsystem
    This commit adds the following significant changes to the Conveyer subsystem:
    - Import `RelativeEncoder` and `SparkLimitSwitch` classes
    - Initialize relative encoders for front and back motors
    - Initialize limit switch objects for beam break sensors
    - Set up enum `NotePosition` with three possible values: NO_NOTE, RECEIVING_NOTE, HELD_NOTE
    - Add method `startReceivingNote()` to set note position to RECEIVING_NOTE and update secondMotorZeroPosition
    - Add method `getNotePosition()` to determine current note position based on motor movement and thresholds
    
    The purpose of these changes is to enhance the functionality of the Conveyer subsystem by incorporating relative encoder readings and limit switch inputs. This will allow for more accurate tracking of note positions during operation.

--- src/main/java/frc/robot/subsystems/conveyer/Conveyer.java
+++ src/main/java/frc/robot/subsystems/conveyer/Conveyer.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.conveyer;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkLimitSwitch;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.utils.SparkMaxUtils;
@@ -12,10 +14,43 @@ public class Conveyer extends SubsystemBase {
       new CANSparkMax(RobotMap.FRONT_CONVEYER_CAN_ID, MotorType.kBrushless);
   private CANSparkMax backMotor =
       new CANSparkMax(RobotMap.BACK_CONVEYER_CAN_ID, MotorType.kBrushless);
-  
+
+  private RelativeEncoder frontMotorEncoder = frontMotor.getEncoder();
+  private RelativeEncoder backMotorEncoder = backMotor.getEncoder();
+
+  /**
+   * These aren't actually limit switches; we just use SparkLimitSwitch objects to access them
+   * easily. At the moment, these are unused.
+   */
+  private SparkLimitSwitch beamBreakSensorOne,
+      beamBreakSensorTwo,
+      beamBreakSensorThree,
+      beamBreakSensorFour;
+
+  public enum NotePosition {
+    NO_NOTE,
+    RECEIVING_NOTE,
+    HELD_NOTE
+  }
+
+  public NotePosition notePosition = NotePosition.NO_NOTE;
+
+  public double secondMotorZeroPosition;
 
   public Conveyer() {
     SparkMaxUtils.initWithRetry(this::setUpConveyerSparks, ConveyerCal.SPARK_INIT_RETRY_ATTEMPTS);
+
+    beamBreakSensorOne = frontMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    beamBreakSensorTwo = frontMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    beamBreakSensorThree = backMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    beamBreakSensorFour = backMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+
+    beamBreakSensorOne.enableLimitSwitch(false);
+    beamBreakSensorTwo.enableLimitSwitch(false);
+    beamBreakSensorThree.enableLimitSwitch(false);
+    beamBreakSensorFour.enableLimitSwitch(false);
+
+    secondMotorZeroPosition = backMotorEncoder.getPosition();
   }
 
   /** Does all the initialization for the sparks, return true on success */
@@ -45,9 +80,9 @@ public class Conveyer extends SubsystemBase {
     backMotor.set(ConveyerCal.PREPARE_TO_SHOOT_BACK_SPEED);
   }
 
-  public void prepareTrapOrAmp() {
-    frontMotor.set(ConveyerCal.PREPARE_TO_AMP_TRAP_FRONT_SPEED);
-    backMotor.set(ConveyerCal.PREPARE_TO_AMP_TRAP_BACK_SPEED);
+  public void scoreTrapOrAmp() {
+    frontMotor.set(ConveyerCal.SCORE_AMP_TRAP_FRONT_SPEED);
+    backMotor.set(ConveyerCal.SCORE_AMP_TRAP_BACK_SPEED);
   }
 
   public void hold() {
@@ -58,5 +93,38 @@ public class Conveyer extends SubsystemBase {
   public void stop() {
     frontMotor.set(0.0);
     backMotor.set(0.0);
+  }
+
+  public void startReceivingNote() {
+    notePosition = NotePosition.RECEIVING_NOTE;
+    secondMotorZeroPosition = backMotorEncoder.getPosition();
+    hold();
+  }
+
+  /**
+   * Get the current position of the note. While we have the beam break sensors available, we are
+   * attempting to not use them, and rather just check the note position with the motors.
+   */
+  public NotePosition getNotePosition() {
+    /**
+     * If the front motor is not moving and we currently have no note, we are not receiving a note
+     */
+    if (notePosition == NotePosition.NO_NOTE && frontMotorEncoder.getVelocity() < 1) {
+      notePosition = NotePosition.NO_NOTE;
+    } else {
+      /** If the back motor has moved more than the threshold, we have a note */
+      if (backMotorEncoder.getPosition() - secondMotorZeroPosition
+          > ConveyerCal.NOTE_THRESHOLD_DEGREES) {
+        notePosition = NotePosition.HELD_NOTE;
+      } else {
+        /**
+         * If we were receiving a note and the back motor has not moved more than the threshold, we
+         * are no still receiving a note
+         */
+        notePosition = NotePosition.RECEIVING_NOTE;
+      }
+    }
+
+    return notePosition;
   }
 }
--- src/main/java/frc/robot/subsystems/conveyer/ConveyerCal.java
+++ src/main/java/frc/robot/subsystems/conveyer/ConveyerCal.java
@@ -9,9 +9,12 @@ public class ConveyerCal {
   public static final double PREPARE_TO_SHOOT_FRONT_SPEED = 1.0, PREPARE_TO_SHOOT_BACK_SPEED = 1.0;
 
   /** The power used for each conveyer motor when moving towards the intake (backwards) */
-  public static final double PREPARE_TO_AMP_TRAP_FRONT_SPEED = -1.0,
-      PREPARE_TO_AMP_TRAP_BACK_SPEED = -1.0;
+  public static final double SCORE_AMP_TRAP_FRONT_SPEED = -1.0,
+      SCORE_AMP_TRAP_BACK_SPEED = -1.0;
 
   /** The power used for each conveyer motor to hold the note in place */
   public static final double FRONT_HOLD_SPEED = 1.0, BACK_HOLD_SPEED = -1.0;
+
+  /** The threshold required for the position of the back motor to change in order to register the note */
+  public static final double NOTE_THRESHOLD_DEGREES = 0.5;
 }
