e7bde5ed8b642d3c2f6d70b43d83c744b7af93c6
Basel Alghanem <basel@umich.edu>
2024-01-28 23:22:30 -0600

    Some cleanup on the elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorCal.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorCal.java
@@ -4,42 +4,46 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import frc.robot.Constants;
 
 public final class ElevatorCal {
+  /** Feedback parameters for elevator when not climbing. Input in/s, output Volts */
   public static final double NOTE_SCORING_P = Constants.PLACEHOLDER_DOUBLE,
       NOTE_SCORING_I = Constants.PLACEHOLDER_DOUBLE,
-      NOTE_SCORING_D = Constants.PLACEHOLDER_DOUBLE,
-      NOTE_SCORING_KS = Constants.PLACEHOLDER_DOUBLE,
+      NOTE_SCORING_D = Constants.PLACEHOLDER_DOUBLE;
+  /** Feedforward parameters for the elevator when not climbing. Input in/s, output Volts. */
+    public static final double NOTE_SCORING_KS = Constants.PLACEHOLDER_DOUBLE,
       NOTE_SCORING_KV = Constants.PLACEHOLDER_DOUBLE,
       NOTE_SCORING_KA = Constants.PLACEHOLDER_DOUBLE;
   public static final SimpleMotorFeedforward NOTE_SCORING_FF =
       new SimpleMotorFeedforward(NOTE_SCORING_KS, NOTE_SCORING_KV, NOTE_SCORING_KA);
 
+  /** Feedback parameters for elevator when climbing. Input in/s, output Volts. */
   public static final double CLIMBING_P = Constants.PLACEHOLDER_DOUBLE,
       CLIMBING_I = Constants.PLACEHOLDER_DOUBLE,
-      CLIMBING_D = Constants.PLACEHOLDER_DOUBLE,
+      CLIMBING_D = Constants.PLACEHOLDER_DOUBLE;
+  /** Feedforward parameters for the elevator when climbing. Input in/s, output Volts. */
+      public static final double
       CLIMBING_KS = Constants.PLACEHOLDER_DOUBLE,
       CLIMBING_KV = Constants.PLACEHOLDER_DOUBLE,
       CLIMBING_KA = Constants.PLACEHOLDER_DOUBLE;
   public static final SimpleMotorFeedforward CLIMBING_FF =
       new SimpleMotorFeedforward(CLIMBING_KS, CLIMBING_KV, CLIMBING_KA);
 
+  /** Elevator trapezoidal motion profile constraints. */
   public static final double MAX_VELOCITY_IN_PER_SECOND = Constants.PLACEHOLDER_DOUBLE,
       MAX_ACCELERATION_IN_PER_SECOND_SQUARED = Constants.PLACEHOLDER_DOUBLE;
-  public static final double POSITION_IN_HOME = Constants.PLACEHOLDER_DOUBLE,
-      POSITION_IN_SCORE_TRAP = Constants.PLACEHOLDER_DOUBLE,
-      POSITION_IN_SCORE_AMP = Constants.PLACEHOLDER_DOUBLE,
-      POSITION_IN_PRE_CLIMB = Constants.PLACEHOLDER_DOUBLE,
-      POSITION_IN_POST_CLIMB = Constants.PLACEHOLDER_DOUBLE;
 
-  public static final int MAX_INIT_RETRY_ATTEMPTS = 5;
+  /** Elevator positions relative to home position */
+  public static final double POSITION_HOME_INCHES = Constants.PLACEHOLDER_DOUBLE,
+      POSITION_SCORE_TRAP_INCHES = Constants.PLACEHOLDER_DOUBLE,
+      POSITION_SCORE_AMP_INCHES = Constants.PLACEHOLDER_DOUBLE,
+      POSITION_PRE_CLIMB_INCHES = Constants.PLACEHOLDER_DOUBLE,
+      POSITION_POST_CLIMB_INCHES = Constants.PLACEHOLDER_DOUBLE;
 
-  public static final double ELEVATOR_LEFT_ABSOLUTE_ENCODER_RATIO = Constants.PLACEHOLDER_DOUBLE,
-      ELEVATOR_RIGHT_ABSOLUTE_ENCODER_RATIO = Constants.PLACEHOLDER_DOUBLE;
-  public static final double ELEVATOR_MOTOR_ENCODER_IN_PER_REV = Constants.PLACEHOLDER_DOUBLE,
-      ELEVATOR_MOTOR_ENCODER_IN_PER_SEC_PER_RPM = Constants.PLACEHOLDER_DOUBLE;
+  /** Soft limits for motors relative to home position. */
   public static final float ELEVATOR_POSITIVE_LIMIT_INCHES = Constants.PLACEHOLDER_FLOAT,
       ELEVATOR_NEGATIVE_LIMIT_INCHES = Constants.PLACEHOLDER_FLOAT;
 
   public static final int ELEVATOR_CURRENT_LIMIT_AMPS = Constants.PLACEHOLDER_INT;
 
-  public static final double ELEVATOR_MARGIN_IN = Constants.PLACEHOLDER_DOUBLE;
+  /** Consider the elevator at the desired position if within this margin of the target. */
+  public static final double ELEVATOR_MARGIN_INCHES = 0.5;
 }
