b8c81e32321cbf0e7294c0f164a9aa17b9ed6b86
itsmesp8343 <sofialpic@gmail.com>
2024-02-03 18:22:42 -0600

    Intake Sequence
    -created the sequence for intaking and checking if the elevator is in right position

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -5,31 +5,39 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.Intake.IntakePosition;
 
 public class IntakeSequence extends SequentialCommandGroup{
   public IntakeSequence(Intake intake, Elevator elevator, Conveyor conveyor) {
     addRequirements(intake, elevator);
 
     addCommands(
-      new ConditionalCommand(new InstantCommand(), new InstantCommand(() -> {elevator.setDesiredPosition(ElevatorPosition.HOME);}, elevator), elevator::atDesiredPosition),
-
-      new WaitUntilCommand(elevator::atDesiredPosition),
+      new ConditionalCommand(new InstantCommand(),new SequentialCommandGroup(
+                new InstantCommand(
+                    () -> intake.setDesiredIntakePosition(IntakePosition.CLEAR_OF_CONVEYOR)),
+                new WaitUntilCommand(intake::atDesiredIntakePosition),
+                new InstantCommand(() -> elevator.setDesiredPosition(ElevatorPosition.HOME)),
+                new WaitUntilCommand(elevator::elevatorBelowInterferenceThreshold)), elevator::atDesiredPosition),
 
       new InstantCommand( 
       () -> {
-        intake.moveToPos(IntakePosition.DEPLOYED);}, intake),
+        intake.setDesiredIntakePosition(IntakePosition.DEPLOYED);}, intake),
 
       new WaitUntilCommand(intake::atDesiredIntakePosition),
 
       new InstantCommand(intake::startRollers),
 
-      new WaitUntilCommand(conveyor::recieve),
+      Conveyor.receive(conveyor),
 
       new InstantCommand( 
       () -> {
-        intake.stopRollers();}, intake)
+        intake.stopRollers();}, intake),
+
+      new InstantCommand(() -> intake.setDesiredIntakePosition(IntakePosition.STOWED))
     );
 
   }
