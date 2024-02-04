32314f603c3147fa77a213e9b13aea6cd468823a
James Hubbard <cobbsaladluv@gmail.com>
2024-02-03 21:01:49 -0600

    Added some comments

--- src/main/java/frc/robot/utils/CRTGearRatioUtil.java
+++ src/main/java/frc/robot/utils/CRTGearRatioUtil.java
@@ -1,8 +1,15 @@
 package frc.robot.utils;
 
-import java.util.Arrays;
+import java.util.Optional;
 
-/*Helpful CRT functions */
+/* Helpful CRT functions - calculate the absolute position of an object attached to a gear
+ * 
+ * How to use
+ * -Initialize the crtutil using the init function
+ * -Then, to get the absolute location of the object attached to the gear, 
+ * call getAbsolutePositionOfMainObject on the util object with the values from the two gear
+ * 
+ */
 public class CRTGearRatioUtil {
   private final double mainGearRotationRatioTerm;
   private final double mainGearRotationRatioTermSimplified;
@@ -11,7 +18,37 @@ public class CRTGearRatioUtil {
   private final int maxRotations;
   private final double mainGearCircumference;
 
-  public CRTGearRatioUtil(int mainGearRotationRatioTerm, int secondaryGearRotationRatioTerm, int maxRotations, double mainGearCircumference) {
+  /**
+   * 
+   * @param mainGearRotationRatioTerm      for every mainGearRotationRatioTerm
+   *                                       rotations of the main gear,
+   *                                       the secondary gear rotates
+   *                                       secondaryGearRotationRatioTerm times
+   * @param secondaryGearRotationRatioTerm for every mainGearRotationRatioTerm
+   *                                       rotations of the main gear,
+   *                                       the secondary gear rotates
+   *                                       secondaryGearRotationRatioTerm times
+   * @param maxRotations                   the maximum number of rotations that
+   *                                       the main gear will make that involve
+   *                                       unique positions of the object
+   *                                       attached to it
+   * @param mainGearCircumference          circumference of the main gear
+   * 
+   * @return Returns optional of the util if the gear ratio works with the number
+   *         of max rotations, otherwise returns an empty optional
+   */
+  public static Optional<CRTGearRatioUtil> init(int mainGearRotationRatioTerm, int secondaryGearRotationRatioTerm,
+      int maxRotations, double mainGearCircumference) {
+    CRTGearRatioUtil u = new CRTGearRatioUtil(mainGearRotationRatioTerm, secondaryGearRotationRatioTerm, maxRotations,
+        mainGearCircumference);
+    if (u.validateGearRatioWithMaxRotations()) {
+      return Optional.of(u);
+    }
+    return Optional.empty();
+  }
+
+  private CRTGearRatioUtil(int mainGearRotationRatioTerm, int secondaryGearRotationRatioTerm, int maxRotations,
+      double mainGearCircumference) {
     this.mainGearRotationRatioTerm = (double) mainGearRotationRatioTerm;
     this.secondaryGearRotationRatioTerm = (double) secondaryGearRotationRatioTerm;
     this.mainGearRotationRatioTermSimplified = (double) (mainGearRotationRatioTerm
@@ -36,6 +73,11 @@ public class CRTGearRatioUtil {
     return r;
   }
 
+  /**
+   * 
+   * @return Returns a multiplier used by CRT. You shouldn't need to use this, and
+   *         I don't fully understand it (Victor Chen did the math behind it)
+   */
   public int kFactor() {
     double sum = 0;
     double a = secondaryGearRotationRatioTermSimplfied;
@@ -47,8 +89,14 @@ public class CRTGearRatioUtil {
     return (int) ((int) ((sum * b) + 1.0) / a);
   }
 
-  // =(mod(GRD, mod(K*(E1*SGRN-E2*SGRD), SGRD)+SGRD))*PI()+E1*PI()
-  // denominator=main gear
+  /**
+   * 
+   * @param mainGearRotationValue      Rotation value returned by the main gear
+   *                                   encoder
+   * @param secondaryGearRotationValue Rotation value returned by the secondary
+   *                                   gear encoder
+   * @return Returns the number of full rotations of the main gear
+   */
   public int getMainGearFullRotations(double mainGearRotationValue, double secondaryGearRotationValue) {
     double k = kFactor();
     return (int) Math.round(mod(mainGearRotationRatioTerm,
@@ -57,28 +105,48 @@ public class CRTGearRatioUtil {
             + mainGearRotationRatioTermSimplified));
   }
 
-  public boolean validateGearRatioWithMaxRotations(){
+  /**
+   * 
+   * @return Returns true if the gear ratio works with the neccessary number of
+   *         max rotations, false otherwise
+   */
+  public boolean validateGearRatioWithMaxRotations() {
     boolean works = true;
-     for (int i = 0; i < maxRotations * 6; i++) {
-     double elevatorPos = i * mainGearCircumference / 6;
-     // System.out.println(elevatorPos);
-     double epoc = CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) >
-     0.9 ? 0.0
-     : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) < 0.1 ? 0.0
-     : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1);
-     if(Math.abs(this.getAbsolutePositionOfMainObject(epoc,
-      CRTGearRatioUtil.mod(
-      elevatorPos / mainGearCircumference * secondaryGearRotationRatioTerm /
-      mainGearRotationRatioTerm,
-      1)) - elevatorPos) > 0.01){
+    for (int i = 0; i < maxRotations * 6; i++) {
+      double elevatorPos = i * mainGearCircumference / 6;
+      // System.out.println(elevatorPos);
+      double epoc = CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) > 0.9 ? 0.0
+          : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) < 0.1 ? 0.0
+              : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1);
+      if (Math.abs(this.getAbsolutePositionOfMainObject(epoc,
+          CRTGearRatioUtil.mod(
+              elevatorPos / mainGearCircumference * secondaryGearRotationRatioTerm /
+                  mainGearRotationRatioTerm,
+              1))
+          - elevatorPos) > 0.01) {
         works = false;
       }
-     }
+    }
     return works;
   }
 
-  public double getAbsolutePositionOfMainObject(double mainGearRotationValue, double secondaryGearRotationValue){
-    return mainGearRotationValue*Math.PI+getMainGearFullRotations(mainGearRotationValue, secondaryGearRotationValue)*Math.PI;
+  /**
+   * 
+   * @param mainGearRotationValue
+   * @param secondaryGearRotationValue
+   * @return Applies CRT to find the absolute position of the object attached to
+   *         the main gear
+   */
+  public double getAbsolutePositionOfMainObject(double mainGearRotationValue, double secondaryGearRotationValue) {
+    return mainGearRotationValue * mainGearCircumference
+        + getMainGearFullRotations(mainGearRotationValue, secondaryGearRotationValue) * mainGearCircumference;
+  }
+
+  /**
+   * @return Returns the absolute position of the main object before it wraps around
+   */
+  public double wrapAroundAbsolutePosition(){
+    return mainGearCircumference*maxRotations;
   }
 
 }
--- src/test/java/CRTGearRatioUtilTestingStuff.java
+++ src/test/java/CRTGearRatioUtilTestingStuff.java
@@ -1,5 +1,6 @@
 import frc.robot.utils.CRTGearRatioUtil;
 import org.junit.jupiter.api.Test;
+import java.util.Optional;
 
 public class CRTGearRatioUtilTestingStuff {
   @Test
@@ -10,18 +11,21 @@ public class CRTGearRatioUtilTestingStuff {
     assert !checker(4,3,10);
     assert checker(28,30,14);
   }
-
+  /*Main gear ratio, secondary gear ratio, max rotations */
   boolean checker(int mgr, int sgr, int mr){
     int mainGearRotationRatioTerm = mgr;
     int secondaryGearRotationRatioTerm = sgr;
     int maxRotations = mr;
     double mainGearCircumference = Math.PI;
-    CRTGearRatioUtil crtutil = new CRTGearRatioUtil(mainGearRotationRatioTerm, secondaryGearRotationRatioTerm, maxRotations,
-        Math.PI);
+    Optional<CRTGearRatioUtil> crtutilOptional = CRTGearRatioUtil.init(mainGearRotationRatioTerm, secondaryGearRotationRatioTerm, maxRotations,
+        mainGearCircumference);
+    if(!crtutilOptional.isPresent()){
+      return false;
+    }
+    CRTGearRatioUtil crtutil = crtutilOptional.get();
     boolean works = true;
      for (int i = 0; i < maxRotations * 6; i++) {
      double elevatorPos = i * mainGearCircumference / 6;
-     // System.out.println(elevatorPos);
      double epoc = CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) >
      0.9 ? 0.0
      : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) < 0.1 ? 0.0
@@ -34,6 +38,6 @@ public class CRTGearRatioUtilTestingStuff {
         works = false;
       }
      }
-     return works && crtutil.validateGearRatioWithMaxRotations();
+     return works;
   }
 }
