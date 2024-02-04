b8275b027488805a3bf3c8e1cb2b71370d11df96
itsmesp8343 <sofialpic@gmail.com>
2024-02-03 18:25:52 -0600

    Formatted

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.conveyor.Conveyor;
@@ -11,34 +10,33 @@ import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.Intake.IntakePosition;
 
-public class IntakeSequence extends SequentialCommandGroup{
+public class IntakeSequence extends SequentialCommandGroup {
   public IntakeSequence(Intake intake, Elevator elevator, Conveyor conveyor) {
     addRequirements(intake, elevator);
 
     addCommands(
-      new ConditionalCommand(new InstantCommand(),new SequentialCommandGroup(
+        new ConditionalCommand(
+            new InstantCommand(),
+            new SequentialCommandGroup(
                 new InstantCommand(
                     () -> intake.setDesiredIntakePosition(IntakePosition.CLEAR_OF_CONVEYOR)),
                 new WaitUntilCommand(intake::atDesiredIntakePosition),
                 new InstantCommand(() -> elevator.setDesiredPosition(ElevatorPosition.HOME)),
-                new WaitUntilCommand(elevator::elevatorBelowInterferenceThreshold)), elevator::atDesiredPosition),
-
-      new InstantCommand( 
-      () -> {
-        intake.setDesiredIntakePosition(IntakePosition.DEPLOYED);}, intake),
-
-      new WaitUntilCommand(intake::atDesiredIntakePosition),
-
-      new InstantCommand(intake::startRollers),
-
-      Conveyor.receive(conveyor),
-
-      new InstantCommand( 
-      () -> {
-        intake.stopRollers();}, intake),
-
-      new InstantCommand(() -> intake.setDesiredIntakePosition(IntakePosition.STOWED))
-    );
-
+                new WaitUntilCommand(elevator::elevatorBelowInterferenceThreshold)),
+            elevator::atDesiredPosition),
+        new InstantCommand(
+            () -> {
+              intake.setDesiredIntakePosition(IntakePosition.DEPLOYED);
+            },
+            intake),
+        new WaitUntilCommand(intake::atDesiredIntakePosition),
+        new InstantCommand(intake::startRollers),
+        Conveyor.receive(conveyor),
+        new InstantCommand(
+            () -> {
+              intake.stopRollers();
+            },
+            intake),
+        new InstantCommand(() -> intake.setDesiredIntakePosition(IntakePosition.STOWED)));
   }
 }
