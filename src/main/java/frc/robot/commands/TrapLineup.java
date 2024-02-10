79be0f6dc9018911fecdade5ccf5229295c8945e
James Hubbard <cobbsaladluv@gmail.com>
2024-02-10 16:46:37 -0600

    Code to pull up to and then line up with the trap

---
+++ src/main/java/frc/robot/commands/TrapLineup.java
@@ -0,0 +1,48 @@
+package frc.robot.commands;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.subsystems.intakeLimelight.IntakeLimelight;
+
+public class TrapLineup extends Command{
+  private DriveSubsystem drive;
+  private IntakeLimelight tagLimelight;
+  private boolean targetLocked = false;
+
+  public TrapLineup(
+      IntakeLimelight limelight, DriveSubsystem driveSubsystem) {
+    // Note: does not require the drive subsystem itself! It just sets the final point in the drive.
+    drive = driveSubsystem;
+    tagLimelight = limelight;
+  }
+
+  @Override
+  public void initialize() {
+    
+  }
+
+  @Override
+  public void execute() {
+    if (!targetLocked) {
+      Optional<Transform2d> robotToScoringLocation = tagLimelight.checkForTag();
+      if (!robotToScoringLocation.isPresent()) {
+        robotToScoringLocation = Optional.empty();
+        return;
+      }
+      double latencySeconds = tagLimelight.getLatencySeconds();
+      targetLocked = true;
+      drive.setLimelightTargetFromTransform(robotToScoringLocation.get(), latencySeconds);
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return targetLocked;
+  }
+}
