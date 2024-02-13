0043068b7b12fab8c9ab9fb8bca0b1458507fce0
voidSevenSevenSix <86328356+voidSevenSevenSix@users.noreply.github.com>
2024-02-12 22:17:58 -0600

    CRT Util (#12)
    * Implemented CRT Util, added a unit test for it. I will add comments on the actual math later.
    
    * Added some comments
    
    * Math comments + one comment correction
    
    * Basel Fixes I (not all fixes have been done yet)
    
    * Fix build fail error
    
    * Basel Fixes II (still not quite done, but class is ending)
    
    * Basel Fixes III
    
    * Basel Fixes IV - did the 10 comments github hid from me
    
    * crt done, uncommenting unit tests
    
    * typo
    
    * Formatting fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/CRTGearRatioUtilTestingStuff.java
@@ -0,0 +1,64 @@
+import frc.robot.utils.CRTGearRatioUtil;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public class CRTGearRatioUtilTestingStuff {
+  @Test
+  void test() {
+    // we know which of these should work based on the wrap around point compared to the maximum
+    // number of rotations we need to do
+    assert checker(44, 42, 22);
+    assert checker(44, 26, 22);
+    assert checker(26, 44, 12);
+    assert checker(42, 44, 20);
+    assert !checker(3, 4, 22);
+    assert !checker(4, 3, 10);
+    assert checker(28, 30, 14);
+  }
+  /**
+   * @param mgr Main gear rotation ratio term
+   * @param sgr Secondary gear rotation ratio term
+   * @param mr Max rotations
+   * @return True if the gear ratio works, false otherwise
+   */
+  boolean checker(int mgr, int sgr, int mr) {
+    int mainGearRotationRatioTerm = mgr;
+    int secondaryGearRotationRatioTerm = sgr;
+    int maxRotations = mr;
+    double mainGearCircumference = Math.PI;
+    Optional<CRTGearRatioUtil> crtUtilOptional =
+        CRTGearRatioUtil.init(
+            mainGearRotationRatioTerm,
+            secondaryGearRotationRatioTerm,
+            maxRotations,
+            mainGearCircumference);
+    if (!crtUtilOptional.isPresent()) {
+      return false;
+    }
+    CRTGearRatioUtil crtutil = crtUtilOptional.get();
+    boolean works = true;
+    for (int i = 0; i < maxRotations * 6; i++) {
+      double elevatorPos = i * mainGearCircumference / 6;
+      double epoc =
+          CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) > 0.9
+              ? 0.0
+              : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1) < 0.1
+                  ? 0.0
+                  : CRTGearRatioUtil.mod(elevatorPos / mainGearCircumference, 1);
+      if (Math.abs(
+              crtutil.getAbsolutePositionOfMainObject(
+                      epoc,
+                      CRTGearRatioUtil.mod(
+                          elevatorPos
+                              / mainGearCircumference
+                              * secondaryGearRotationRatioTerm
+                              / mainGearRotationRatioTerm,
+                          1))
+                  - elevatorPos)
+          > 0.01) {
+        works = false;
+      }
+    }
+    return works;
+  }
+}
