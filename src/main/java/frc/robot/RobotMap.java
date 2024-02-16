b2cfdf13fa1d6c047072888212562fb77a6c9919
Basel Alghanem <basel@umich.edu>
2024-02-16 17:51:07 -0600

    set can ids

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,36 +1,37 @@
 package frc.robot;
 
 public class RobotMap {
-  public static final int PIGEON_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int PIGEON_CAN_ID = 20;
   /** Driving TalonFX CAN IDs */
-  public static final int FRONT_LEFT_DRIVING_CAN_ID = Constants.PLACEHOLDER_INT,
-      REAR_LEFT_DRIVING_CAN_ID = Constants.PLACEHOLDER_INT,
-      FRONT_RIGHT_DRIVING_CAN_ID = Constants.PLACEHOLDER_INT,
-      REAR_RIGHT_DRIVING_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int FRONT_LEFT_DRIVING_CAN_ID = 11,
+      REAR_LEFT_DRIVING_CAN_ID = 16,
+      FRONT_RIGHT_DRIVING_CAN_ID = 6,
+      REAR_RIGHT_DRIVING_CAN_ID = 2;
 
   /** shooter SPARK MAX CAN IDS* */
-  public static final int SHOOTER_MOTOR_A_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int SHOOTER_MOTOR_RIGHT_CAN_ID = 4;
 
-  public static final int SHOOTER_MOTOR_B_CAN_ID = Constants.PLACEHOLDER_INT;
-  public static final int SHOOTER_PIVOT_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int SHOOTER_MOTOR_LEFT_ONE_CAN_ID = 13;
+  public static final int SHOOTER_MOTOR_LEFT_TWO_CAN_ID = 14;
+  public static final int SHOOTER_PIVOT_MOTOR_CAN_ID = 9;
 
   /** Turning SPARK MAX CAN IDs */
-  public static final int FRONT_LEFT_TURNING_CAN_ID = Constants.PLACEHOLDER_INT,
-      REAR_LEFT_TURNING_CAN_ID = Constants.PLACEHOLDER_INT,
-      FRONT_RIGHT_TURNING_CAN_ID = Constants.PLACEHOLDER_INT,
-      REAR_RIGHT_TURNING_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int FRONT_LEFT_TURNING_CAN_ID = 12,
+      REAR_LEFT_TURNING_CAN_ID = 15,
+      FRONT_RIGHT_TURNING_CAN_ID = 5,
+      REAR_RIGHT_TURNING_CAN_ID = 3;
 
   /** Conveyor SPARK MAX CAN IDs */
-  public static final int FRONT_CONVEYOR_CAN_ID = Constants.PLACEHOLDER_INT,
-      BACK_CONVEYOR_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int FRONT_CONVEYOR_CAN_ID = 18,
+      BACK_CONVEYOR_CAN_ID = 19;
 
-  public static final int LEFT_ELEVATOR_CAN_ID = Constants.PLACEHOLDER_INT,
-      RIGHT_ELEVATOR_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int LEFT_ELEVATOR_CAN_ID = 10,
+      RIGHT_ELEVATOR_CAN_ID = 7;
 
   /** Intake motor and talon CAN IDs */
-  public static final int INTAKE_PIVOT_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT,
-      INTAKING_FRONT_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT,
-      INTAKING_BACK_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT;
+  public static final int INTAKE_PIVOT_MOTOR_CAN_ID = 8,
+      INTAKING_LEFT_MOTOR_CAN_ID = 22,
+      INTAKING_RIGHT_MOTOR_CAN_ID = 21;
 
   public static final int CANDLE_CAN_ID = Constants.PLACEHOLDER_INT;
 }
