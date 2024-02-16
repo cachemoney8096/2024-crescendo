c5553d7d01997a8a06b8cc56bcb1e9b56c9134b7
Asher Grossman <asher.grossman@gmail.com>
2024-02-15 18:22:50 -0600

    Added shooter LL (identical to intake LL, just renamed "intake" stuff)

---
+++ src/main/java/frc/robot/subsystems/shooterLimelight/ShooterLimelightCal.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.shooterLimelight;
+
+public class ShooterLimelightCal {
+  public static final double LIMELIGHT_YAW_DEGREES = 0.0;
+}
---
+++ src/main/java/frc/robot/subsystems/shooterLimelight/ShooterLimelightConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.shooterLimelight;
+
+import frc.robot.Constants;
+
+public class ShooterLimelightConstants {
+  public static final String SHOOTER_LIMELIGHT_NAME = "limelight-shooter";
+
+  public static final double SHOOTER_LIMELIGHT_PITCH_DEGREES = Constants.PLACEHOLDER_DOUBLE;
+  public static final double SHOOTER_LIMELIGHT_HEIGHT_METERS = Constants.PLACEHOLDER_DOUBLE;
+}
