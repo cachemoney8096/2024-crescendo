2428ff3141e03a7471a060cae3c9bc3e841da37a
SaanikaGW <151690212+SaanikaGW@users.noreply.github.com>
2024-02-17 15:40:39 -0600

    update Amp Shoot Command (#35)
    * update Amp Shoot Command
    
    Making it so amp shoot only waits for intake to be clear of the conveyor intead of waiting for the intake to be fully stowed
    
    * Update AmpPrepScore.java
    
    * PR comment
    
    ---------
    
    Co-authored-by: Allison Li <ali@ucls.uchicago.edu>

--- src/main/java/frc/robot/commands/AmpScore.java
+++ src/main/java/frc/robot/commands/AmpScore.java
@@ -1,7 +1,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.intake.Intake;
 
 /**
  * Scores a note in the amp. At the moment, this only calls the scoreTrapOrAmp command in the
@@ -9,9 +12,12 @@ import frc.robot.subsystems.conveyor.Conveyor;
  * we will likely want a separate command group.
  */
 public class AmpScore extends SequentialCommandGroup {
-  public AmpScore(Conveyor conveyor) {
-    addRequirements(conveyor);
+  public AmpScore(Conveyor conveyor, Intake intake, Elevator elevator) {
+    addRequirements(conveyor, intake, elevator);
     addCommands(
+      new WaitUntilCommand(() -> {
+              return intake.clearOfConveyorZone() && elevator.atDesiredPosition();
+            }),
         Conveyor.scoreTrapOrAmp(conveyor)
         // TODO "smartly" go home (e.g. go home once we've driven away from the wall)
         );
