f59c18b5fa96eaf5f4aa228f1e6936a7e400733d
allison324 <124639587+allison324@users.noreply.github.com>
2024-02-17 15:39:34 -0600

    Light Class (#28)
    * Light Class
    
    We made Light class and mainly copied everything from last year's code(same functions and everything) - the only things that we changed are the colors and the enums. Also added a const int for the canid in RobotMap.java.
    
    * Light class
    
    minor changes, add lights constants
    
    * Lights-class
    
    Minor changes based on Ava's comments.
    
    * LightsClassUpdates
    
    I just went through all of the comments written by Ava and Asher the appropriate changes.
    
    * Light class, added NO_TAG enum
    
    * Formatting fixes
    
    * delete drivetopoint
    
    ---------
    
    Co-authored-by: ruben-hayrapetyan <ruben.hayrapetyan28@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/lights/Lights.java
@@ -0,0 +1,82 @@
+package frc.robot.subsystems.lights;
+
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import frc.robot.RobotMap;
+import java.util.TreeMap;
+
+public class Lights {
+  /**
+   * Tree map of light code enums and integers that represent the R, G, B values of each light code
+   * enum
+   */
+  private TreeMap<LightCode, Integer[]> lightOptionsMap;
+
+  private CANdle candle = new CANdle(RobotMap.CANDLE_CAN_ID);
+  private CANdleConfiguration config = new CANdleConfiguration();
+  private LightCode currentLightStatus = LightCode.OFF;
+
+  public enum LightCode {
+    NOTELESS, // red
+    INTAKING, // blue
+    READY_TO_SHOOT, // green
+    ALIGNING_TO_TAG, // orange
+    HOLDING_NOTE, // yellow
+    OFF, // no lights are on
+    NO_TAG,
+    PARTY_MODE;
+  }
+
+  public Lights() {
+    config.stripType = LEDStripType.RGB;
+    config.brightnessScalar = 1.0;
+    candle.configAllSettings(config);
+
+    lightOptionsMap = new TreeMap<LightCode, Integer[]>();
+    lightOptionsMap.put(LightCode.NOTELESS, new Integer[] {255, 0, 0});
+    lightOptionsMap.put(LightCode.INTAKING, new Integer[] {0, 0, 255});
+    lightOptionsMap.put(LightCode.READY_TO_SHOOT, new Integer[] {0, 255, 0});
+    lightOptionsMap.put(LightCode.ALIGNING_TO_TAG, new Integer[] {255, 165, 0});
+    lightOptionsMap.put(LightCode.HOLDING_NOTE, new Integer[] {255, 255, 102});
+    lightOptionsMap.put(LightCode.OFF, new Integer[] {0, 0, 0});
+  }
+
+  public void toggleCode(LightCode light) {
+    if (currentLightStatus == light) {
+      currentLightStatus = LightCode.OFF;
+    } else {
+      currentLightStatus = light;
+    }
+    setLEDs();
+  }
+
+  private void setLEDs() {
+    if (currentLightStatus == LightCode.PARTY_MODE) {
+      setPartyMode();
+    } else if (currentLightStatus == LightCode.NO_TAG) {
+      setNoTag();
+    } else {
+      candle.setLEDs(
+          lightOptionsMap.get(currentLightStatus)[0],
+          lightOptionsMap.get(currentLightStatus)[1],
+          lightOptionsMap.get(currentLightStatus)[2]);
+    }
+  }
+
+  private void setNoTag() {
+    StrobeAnimation strobeAnim = new StrobeAnimation(255, 0, 255);
+    candle.animate(strobeAnim);
+  }
+
+  public void setPartyMode() {
+    RainbowAnimation rainbowAnim =
+        new RainbowAnimation(
+            LightsConstants.LIGHT_BRIGHTNESS,
+            LightsConstants.LIGHT_SPEED,
+            LightsConstants.NUM_CANDLE_LEDS);
+    candle.animate(rainbowAnim);
+  }
+}
