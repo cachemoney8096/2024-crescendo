ca1e5b9d629c126057bb238775d66a6b2429c5bc
itsmesp8343 <114835609+itsmesp8343@users.noreply.github.com>
2024-02-17 15:39:55 -0600

    Set Brownout Voltage (#34)
    * Set Brownout Voltage
    
    set brownout voltage to 5
    
    * Update
    
    made var in constants
    
    ---------
    
    Co-authored-by: allison324 <124639587+allison324@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -31,6 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    RobotController.setBrownoutVoltage(Constants.BROWNOUT_VOLTAGE);
   }
 
   /**
