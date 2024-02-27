c38f9577d039eb19fb4c41a15065524361c2c356
James Hubbard <cobbsaladluv@gmail.com>
2024-02-27 16:59:33 -0600

    Added unclimb command

---
+++ src/main/java/frc/robot/commands/UnclimbSequence.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.Shooter.ShooterMode;
+
+public class UnclimbSequence extends SequentialCommandGroup{
+  public UnclimbSequence(Elevator elevator, Shooter shooter){
+    addRequirements(elevator, shooter);
+    addCommands(
+      new InstantCommand(()->{elevator.setDesiredPosition(ElevatorPosition.HOME, false); }),
+      new WaitUntilCommand(elevator::atDesiredPosition),
+      new InstantCommand(()->{shooter.setShooterMode(ShooterMode.PRELATCH);}),
+      new WaitUntilCommand(shooter::atDesiredPosition), 
+      new InstantCommand(()->{elevator.setDesiredPosition(ElevatorPosition.SCORE_TRAP, false);})
+    );
+  }
+}
