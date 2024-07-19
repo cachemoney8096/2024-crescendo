3e5bba4ebefd4f16c2d0f351b0290cb8e7d14eda
cobbsaladluv <cobbsaladluv@gmail.com>
2024-07-19 12:28:14 -0500

    odometry fix

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -269,15 +269,15 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void resetOdometryToCenterSubwoofer() {
-    double curYawDeg = gyro.getYaw().getValue();
-    double offsetToTargetDeg = targetHeadingDegrees - curYawDeg;
     double odometryXMeters = matchState.isBlue() ? 1.168 : 15.429;
     double odometryYMeters = matchState.isBlue() ? 5.467 : 5.422;
-    double odometryYawDeg = matchState.isBlue() ? 5.162 : -174.872;
-    System.out.println(odometryXMeters + "x " + odometryYMeters + "y " + odometryYawDeg + "deg");
-    odometry.resetPosition(Rotation2d.fromDegrees(odometryYawDeg), getModulePositions(), new Pose2d(odometryXMeters, odometryYMeters, Rotation2d.fromDegrees(odometryYawDeg)));
-    gyro.setYaw(odometryYawDeg);
-    targetHeadingDegrees = odometryYawDeg + offsetToTargetDeg;
+    double curYawDeg = gyro.getYaw().getValue();
+    double offsetToTargetDeg = targetHeadingDegrees - curYawDeg;
+    double yawDeg = matchState.isBlue() ? 0 : 180;
+    gyro.setYaw(yawDeg);
+    Pose2d resetPose = new Pose2d(new Translation2d(odometryXMeters, odometryYMeters), Rotation2d.fromDegrees(yawDeg));
+    odometry.resetPosition(Rotation2d.fromDegrees(yawDeg), getModulePositions(), resetPose);
+    targetHeadingDegrees = yawDeg + offsetToTargetDeg;
   }
 
   /**
